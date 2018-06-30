9eec006f404975b2669da155240ca3e9f2ba859f
Prathik Sai <prathiksai31@gmail.com>
2018-06-30 23:37:37 +0530

    Minor fix

--- mod4_assignment/SpeakHello.js
+++ mod4_assignment/SpeakHello.js
@@ -1,10 +1,7 @@
-
 (function (window) {
-	
-var helloSpeaker = {};
- 
-  var speakWord = "Hello";
-  helloSpeaker.speak = function (name) {
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 window.helloSpeaker = helloSpeaker;
